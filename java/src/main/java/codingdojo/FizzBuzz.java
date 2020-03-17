82edfab61b8b43fd76ee86a8728a1ea20cb5fd8c
Anis CHAABANI <chaabani.anis@gmail.com>
2020-03-17 13:05:50 +0100

    Starter java

---
+++ java/src/main/java/codingdojo/FizzBuzz.java
@@ -0,0 +1,7 @@
+package codingdojo;
+
+public class FizzBuzz {
+    public static String print(String input){
+        return input;
+    }
+}
---
+++ java/src/test/java/codingdojo/FizzBuzzTest.java
@@ -0,0 +1,12 @@
+package codingdojo;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class FizzBuzzTest {
+
+    @Test
+    public void Should_() {
+        assertEquals("1", FizzBuzz.print("1"));
+    }
+}
